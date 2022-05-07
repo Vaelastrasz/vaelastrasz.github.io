cbc4f4d8e24a06cff0e01c3af8d8818f29bafc84
Vlad Chobanitsa <Chobanitsavlad@gmail.com>
2022-05-07 12:28:27 +0300

    Reacts

--- js/data_reaction.js
+++ js/data_reaction.js
@@ -1,51 +1,51 @@
 data_reaction = [
     {
-        title: "Opportunity attack",
+        title: "Атака по возможности",
         icon: "crossed-swords",
-        subtitle: "Enemy leaves your reach",
-        description: "You can rarely move heedlessly past your foes without putting yourself in danger",
+        subtitle: "Враг покидает вашу зону досягаемости",
+        description: "Вряд ли вы сможете двигаться возле врага, не подвергая себя опасности",
         reference: "Adventurer's Guide, pg. 446.",
         bullets: [
-            "Trigger: enemy creature you can see leaves your reach.",
-            "Make one melee attack against the provoking creature.",
-            "The attack interrupts the provoking creature's movement, occurring right before the creature leaves your reach.",
-            "Creatures don't provoke an opportunity attack when they teleport or when someone or something moves them without using their movement, action, or reaction."
+            "Условие: вражеское существо, которое вы видите, покидает вашу зону досягаемости.",
+            "Совершите один удар ближнего боя против провоцирующего существа.",
+            "Атака прерывает движение существа, совершаясь прямо перед тем как цель выйдет из зоны досягаемости.",
+            "Существа не провоцируют атаку по возможности когда они телепортируются либо когда кто-то двигается без использования движения, действия, либо реакции."
         ]
     },
     {
-        title: "Readied action",
+        title: "Подготовленное действие",
         icon: "stopwatch",
-        subtitle: "Part of your Ready action",
-        description: "Execute the reaction specified by your Ready action",
+        subtitle: "Часть вашего подготовленного действия",
+        description: "Исполните реакцию, которую вы подготовили",
         reference: "Adventurer's Guide, pg. 443.",
         bullets: [
-            "Trigger: specified by your <i>Ready</i> action."
+            "Условие: описано вашим действием <i>Подготовка</i>."
         ]
     },
     {
-        title: "Fall Back",
+        title: "Отход",
         icon: "fast-backward-button",
-        subtitle: "Fall back when pressed",
-        description: "The target of a Press the Attack action can fall back",
+        subtitle: "Отойдите против Натиска",
+        description: "Цель Натиска может совершить Отход",
         reference: "Adventurer's Guide, pg. 443",
         bullets: [
-            "When you are the target of a *Press the Attack* action, you can use your reaction to yield ground",
-            "You move backwards 5ft",
-            "Your attacker does not gain an expertise die against you from their Press the Attack",
-            "Your attacker can move 5 feet towards you if you fall back",
-            "Neither you, nor your attacker, provoke opportunity attacks from this movement",
-            "A creature using the Rage class feature cannot choose to Fall back"
+            "Когда вы являетесь целью Натиска, вы можете использовать реакцию чтобы отойти",
+            "Вы отходите назад на 5 футов",
+            "Противник не получает куб экспертизы против вас от своего Натиска",
+            "Нападающий может подойти на 5 футов к вам если вы Отошли",
+            "Ни вы, ни ваш противник не провоцируете Атаку по возможности от этого движения",
+            "Существо, использующее классовую особеность Ярость не может Отойти."
         ]
     },
     {
-        title: "Cast a spell",
+        title: "Наложение заклинания",
         icon: "magic-swirl",
-        subtitle: "Cast time of 1 reaction",
-        description: "Cast a spell with a casting time of 1 reaction",
+        subtitle: "Наложите заклинание за реакцию",
+        description: "Наложите заклинание с временем каста в 1 реакцию",
         reference: "Adventurer's Guide, pg. 442, 491.",
         bullets: [
-            "Trigger: specified by the spell.",
-            "For further details, see the <i>Cast a spell</i> action."
+            "Условие: описано в заклинании.",
+            "Для подробностей, см. действие <i>Наложение заклинания</i>."
         ]
     }
 ]
